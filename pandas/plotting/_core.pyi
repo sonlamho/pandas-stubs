aeb0b7990fa543a14c1dba109b2dd1fc391a38af
Lam Son Ho <holamson2000@gmail.com>
2020-10-02 11:33:52 +0700

    fix syntax error and apply black

--- pandas/plotting/_core.pyi
+++ pandas/plotting/_core.pyi
@@ -37,7 +37,34 @@ class MPLPlot:
     colormap: Any = ...
     table: Any = ...
     kwds: Any = ...
-    def __init__(self, data: Any, kind: Optional[Any] = ..., by: Optional[Any] = ..., subplots: bool = ..., sharex: Optional[Any] = ..., sharey: bool = ..., use_index: bool = ..., figsize: Optional[Any] = ..., grid: Optional[Any] = ..., legend: bool = ..., rot: Optional[Any] = ..., ax: Optional[Any] = ..., fig: Optional[Any] = ..., title: Optional[Any] = ..., xlim: Optional[Any] = ..., ylim: Optional[Any] = ..., xticks: Optional[Any] = ..., yticks: Optional[Any] = ..., sort_columns: bool = ..., fontsize: Optional[Any] = ..., secondary_y: bool = ..., colormap: Optional[Any] = ..., table: bool = ..., layout: Optional[Any] = ..., **kwds: Any) -> None: ...
+    def __init__(
+        self,
+        data: Any,
+        kind: Optional[Any] = ...,
+        by: Optional[Any] = ...,
+        subplots: bool = ...,
+        sharex: Optional[Any] = ...,
+        sharey: bool = ...,
+        use_index: bool = ...,
+        figsize: Optional[Any] = ...,
+        grid: Optional[Any] = ...,
+        legend: bool = ...,
+        rot: Optional[Any] = ...,
+        ax: Optional[Any] = ...,
+        fig: Optional[Any] = ...,
+        title: Optional[Any] = ...,
+        xlim: Optional[Any] = ...,
+        ylim: Optional[Any] = ...,
+        xticks: Optional[Any] = ...,
+        yticks: Optional[Any] = ...,
+        sort_columns: bool = ...,
+        fontsize: Optional[Any] = ...,
+        secondary_y: bool = ...,
+        colormap: Optional[Any] = ...,
+        table: bool = ...,
+        layout: Optional[Any] = ...,
+        **kwds: Any
+    ) -> None: ...
     @property
     def nseries(self): ...
     def draw(self) -> None: ...
@@ -58,11 +85,21 @@ class PlanePlot(MPLPlot):
 
 class ScatterPlot(PlanePlot):
     c: Any = ...
-    def __init__(self, data: Any, x: Any, y: Any, s: Optional[Any] = ..., c: Optional[Any] = ..., **kwargs: Any) -> None: ...
+    def __init__(
+        self,
+        data: Any,
+        x: Any,
+        y: Any,
+        s: Optional[Any] = ...,
+        c: Optional[Any] = ...,
+        **kwargs: Any
+    ) -> None: ...
 
 class HexBinPlot(PlanePlot):
     C: Any = ...
-    def __init__(self, data: Any, x: Any, y: Any, C: Optional[Any] = ..., **kwargs: Any) -> None: ...
+    def __init__(
+        self, data: Any, x: Any, y: Any, C: Optional[Any] = ..., **kwargs: Any
+    ) -> None: ...
 
 class LinePlot(MPLPlot):
     orientation: str = ...
@@ -91,7 +128,9 @@ class BarhPlot(BarPlot):
 class HistPlot(LinePlot):
     bins: Any = ...
     bottom: Any = ...
-    def __init__(self, data: Any, bins: int = ..., bottom: int = ..., **kwargs: Any) -> None: ...
+    def __init__(
+        self, data: Any, bins: int = ..., bottom: int = ..., **kwargs: Any
+    ) -> None: ...
     @property
     def orientation(self): ...
 
@@ -99,14 +138,20 @@ class KdePlot(HistPlot):
     orientation: str = ...
     bw_method: Any = ...
     ind: Any = ...
-    def __init__(self, data: Any, bw_method: Optional[Any] = ..., ind: Optional[Any] = ..., **kwargs: Any) -> None: ...
+    def __init__(
+        self,
+        data: Any,
+        bw_method: Optional[Any] = ...,
+        ind: Optional[Any] = ...,
+        **kwargs: Any
+    ) -> None: ...
 
 class PiePlot(MPLPlot):
     def __init__(self, data: Any, kind: Optional[Any] = ..., **kwargs: Any) -> None: ...
 
 class BoxPlot(LinePlot):
 
-BP = namedtuple('Boxplot', ['ax', 'lines'])
+    BP = namedtuple("Boxplot", ["ax", "lines"])
     return_type: Any = ...
     def __init__(self, data: Any, return_type: str = ..., **kwargs: Any) -> None: ...
     def maybe_color_bp(self, bp: Any) -> None: ...
@@ -126,42 +171,268 @@ series_ax: str
 df_note: str
 series_note: str
 
-def plot_frame(data: Any, x: Optional[Any] = ..., y: Optional[Any] = ..., kind: str = ..., ax: Optional[Any] = ..., subplots: bool = ..., sharex: Optional[Any] = ..., sharey: bool = ..., layout: Optional[Any] = ..., figsize: Optional[Any] = ..., use_index: bool = ..., title: Optional[Any] = ..., grid: Optional[Any] = ..., legend: bool = ..., style: Optional[Any] = ..., logx: bool = ..., logy: bool = ..., loglog: bool = ..., xticks: Optional[Any] = ..., yticks: Optional[Any] = ..., xlim: Optional[Any] = ..., ylim: Optional[Any] = ..., rot: Optional[Any] = ..., fontsize: Optional[Any] = ..., colormap: Optional[Any] = ..., table: bool = ..., yerr: Optional[Any] = ..., xerr: Optional[Any] = ..., secondary_y: bool = ..., sort_columns: bool = ..., **kwds: Any): ...
-def plot_series(data: Any, kind: str = ..., ax: Optional[Any] = ..., figsize: Optional[Any] = ..., use_index: bool = ..., title: Optional[Any] = ..., grid: Optional[Any] = ..., legend: bool = ..., style: Optional[Any] = ..., logx: bool = ..., logy: bool = ..., loglog: bool = ..., xticks: Optional[Any] = ..., yticks: Optional[Any] = ..., xlim: Optional[Any] = ..., ylim: Optional[Any] = ..., rot: Optional[Any] = ..., fontsize: Optional[Any] = ..., colormap: Optional[Any] = ..., table: bool = ..., yerr: Optional[Any] = ..., xerr: Optional[Any] = ..., label: Optional[Any] = ..., secondary_y: bool = ..., **kwds: Any): ...
-def boxplot(data: Any, column: Optional[Any] = ..., by: Optional[Any] = ..., ax: Optional[Any] = ..., fontsize: Optional[Any] = ..., rot: int = ..., grid: bool = ..., figsize: Optional[Any] = ..., layout: Optional[Any] = ..., return_type: Optional[Any] = ..., **kwds: Any): ...
-def boxplot_frame(self, column: Optional[Any] = ..., by: Optional[Any] = ..., ax: Optional[Any] = ..., fontsize: Optional[Any] = ..., rot: int = ..., grid: bool = ..., figsize: Optional[Any] = ..., layout: Optional[Any] = ..., return_type: Optional[Any] = ..., **kwds: Any): ...
-def scatter_plot(data: Any, x: Any, y: Any, by: Optional[Any] = ..., ax: Optional[Any] = ..., figsize: Optional[Any] = ..., grid: bool = ..., **kwargs: Any): ...
-def hist_frame(data: Any, column: Optional[Any] = ..., by: Optional[Any] = ..., grid: bool = ..., xlabelsize: Optional[Any] = ..., xrot: Optional[Any] = ..., ylabelsize: Optional[Any] = ..., yrot: Optional[Any] = ..., ax: Optional[Any] = ..., sharex: bool = ..., sharey: bool = ..., figsize: Optional[Any] = ..., layout: Optional[Any] = ..., bins: int = ..., **kwds: Any): ...
-def hist_series(self, by: Optional[Any] = ..., ax: Optional[Any] = ..., grid: bool = ..., xlabelsize: Optional[Any] = ..., xrot: Optional[Any] = ..., ylabelsize: Optional[Any] = ..., yrot: Optional[Any] = ..., figsize: Optional[Any] = ..., bins: int = ..., **kwds: Any): ...
-def grouped_hist(data: Any, column: Optional[Any] = ..., by: Optional[Any] = ..., ax: Optional[Any] = ..., bins: int = ..., figsize: Optional[Any] = ..., layout: Optional[Any] = ..., sharex: bool = ..., sharey: bool = ..., rot: int = ..., grid: bool = ..., xlabelsize: Optional[Any] = ..., xrot: Optional[Any] = ..., ylabelsize: Optional[Any] = ..., yrot: Optional[Any] = ..., **kwargs: Any): ...
-def boxplot_frame_groupby(grouped: Any, subplots: bool = ..., column: Optional[Any] = ..., fontsize: Optional[Any] = ..., rot: int = ..., grid: bool = ..., ax: Optional[Any] = ..., figsize: Optional[Any] = ..., layout: Optional[Any] = ..., sharex: bool = ..., sharey: bool = ..., **kwds: Any): ...
+def plot_frame(
+    data: Any,
+    x: Optional[Any] = ...,
+    y: Optional[Any] = ...,
+    kind: str = ...,
+    ax: Optional[Any] = ...,
+    subplots: bool = ...,
+    sharex: Optional[Any] = ...,
+    sharey: bool = ...,
+    layout: Optional[Any] = ...,
+    figsize: Optional[Any] = ...,
+    use_index: bool = ...,
+    title: Optional[Any] = ...,
+    grid: Optional[Any] = ...,
+    legend: bool = ...,
+    style: Optional[Any] = ...,
+    logx: bool = ...,
+    logy: bool = ...,
+    loglog: bool = ...,
+    xticks: Optional[Any] = ...,
+    yticks: Optional[Any] = ...,
+    xlim: Optional[Any] = ...,
+    ylim: Optional[Any] = ...,
+    rot: Optional[Any] = ...,
+    fontsize: Optional[Any] = ...,
+    colormap: Optional[Any] = ...,
+    table: bool = ...,
+    yerr: Optional[Any] = ...,
+    xerr: Optional[Any] = ...,
+    secondary_y: bool = ...,
+    sort_columns: bool = ...,
+    **kwds: Any
+): ...
+def plot_series(
+    data: Any,
+    kind: str = ...,
+    ax: Optional[Any] = ...,
+    figsize: Optional[Any] = ...,
+    use_index: bool = ...,
+    title: Optional[Any] = ...,
+    grid: Optional[Any] = ...,
+    legend: bool = ...,
+    style: Optional[Any] = ...,
+    logx: bool = ...,
+    logy: bool = ...,
+    loglog: bool = ...,
+    xticks: Optional[Any] = ...,
+    yticks: Optional[Any] = ...,
+    xlim: Optional[Any] = ...,
+    ylim: Optional[Any] = ...,
+    rot: Optional[Any] = ...,
+    fontsize: Optional[Any] = ...,
+    colormap: Optional[Any] = ...,
+    table: bool = ...,
+    yerr: Optional[Any] = ...,
+    xerr: Optional[Any] = ...,
+    label: Optional[Any] = ...,
+    secondary_y: bool = ...,
+    **kwds: Any
+): ...
+def boxplot(
+    data: Any,
+    column: Optional[Any] = ...,
+    by: Optional[Any] = ...,
+    ax: Optional[Any] = ...,
+    fontsize: Optional[Any] = ...,
+    rot: int = ...,
+    grid: bool = ...,
+    figsize: Optional[Any] = ...,
+    layout: Optional[Any] = ...,
+    return_type: Optional[Any] = ...,
+    **kwds: Any
+): ...
+def boxplot_frame(
+    self,
+    column: Optional[Any] = ...,
+    by: Optional[Any] = ...,
+    ax: Optional[Any] = ...,
+    fontsize: Optional[Any] = ...,
+    rot: int = ...,
+    grid: bool = ...,
+    figsize: Optional[Any] = ...,
+    layout: Optional[Any] = ...,
+    return_type: Optional[Any] = ...,
+    **kwds: Any
+): ...
+def scatter_plot(
+    data: Any,
+    x: Any,
+    y: Any,
+    by: Optional[Any] = ...,
+    ax: Optional[Any] = ...,
+    figsize: Optional[Any] = ...,
+    grid: bool = ...,
+    **kwargs: Any
+): ...
+def hist_frame(
+    data: Any,
+    column: Optional[Any] = ...,
+    by: Optional[Any] = ...,
+    grid: bool = ...,
+    xlabelsize: Optional[Any] = ...,
+    xrot: Optional[Any] = ...,
+    ylabelsize: Optional[Any] = ...,
+    yrot: Optional[Any] = ...,
+    ax: Optional[Any] = ...,
+    sharex: bool = ...,
+    sharey: bool = ...,
+    figsize: Optional[Any] = ...,
+    layout: Optional[Any] = ...,
+    bins: int = ...,
+    **kwds: Any
+): ...
+def hist_series(
+    self,
+    by: Optional[Any] = ...,
+    ax: Optional[Any] = ...,
+    grid: bool = ...,
+    xlabelsize: Optional[Any] = ...,
+    xrot: Optional[Any] = ...,
+    ylabelsize: Optional[Any] = ...,
+    yrot: Optional[Any] = ...,
+    figsize: Optional[Any] = ...,
+    bins: int = ...,
+    **kwds: Any
+): ...
+def grouped_hist(
+    data: Any,
+    column: Optional[Any] = ...,
+    by: Optional[Any] = ...,
+    ax: Optional[Any] = ...,
+    bins: int = ...,
+    figsize: Optional[Any] = ...,
+    layout: Optional[Any] = ...,
+    sharex: bool = ...,
+    sharey: bool = ...,
+    rot: int = ...,
+    grid: bool = ...,
+    xlabelsize: Optional[Any] = ...,
+    xrot: Optional[Any] = ...,
+    ylabelsize: Optional[Any] = ...,
+    yrot: Optional[Any] = ...,
+    **kwargs: Any
+): ...
+def boxplot_frame_groupby(
+    grouped: Any,
+    subplots: bool = ...,
+    column: Optional[Any] = ...,
+    fontsize: Optional[Any] = ...,
+    rot: int = ...,
+    grid: bool = ...,
+    ax: Optional[Any] = ...,
+    figsize: Optional[Any] = ...,
+    layout: Optional[Any] = ...,
+    sharex: bool = ...,
+    sharey: bool = ...,
+    **kwds: Any
+): ...
 
 class BasePlotMethods(PandasObject):
     def __init__(self, data: Any) -> None: ...
     def __call__(self, *args: Any, **kwargs: Any) -> None: ...
 
 class SeriesPlotMethods(BasePlotMethods):
-    def __call__(self, kind: str = ..., ax: Optional[Any] = ..., figsize: Optional[Any] = ..., use_index: bool = ..., title: Optional[Any] = ..., grid: Optional[Any] = ..., legend: bool = ..., style: Optional[Any] = ..., logx: bool = ..., logy: bool = ..., loglog: bool = ..., xticks: Optional[Any] = ..., yticks: Optional[Any] = ..., xlim: Optional[Any] = ..., ylim: Optional[Any] = ..., rot: Optional[Any] = ..., fontsize: Optional[Any] = ..., colormap: Optional[Any] = ..., table: bool = ..., yerr: Optional[Any] = ..., xerr: Optional[Any] = ..., label: Optional[Any] = ..., secondary_y: bool = ..., **kwds: Any): ...
+    def __call__(
+        self,
+        kind: str = ...,
+        ax: Optional[Any] = ...,
+        figsize: Optional[Any] = ...,
+        use_index: bool = ...,
+        title: Optional[Any] = ...,
+        grid: Optional[Any] = ...,
+        legend: bool = ...,
+        style: Optional[Any] = ...,
+        logx: bool = ...,
+        logy: bool = ...,
+        loglog: bool = ...,
+        xticks: Optional[Any] = ...,
+        yticks: Optional[Any] = ...,
+        xlim: Optional[Any] = ...,
+        ylim: Optional[Any] = ...,
+        rot: Optional[Any] = ...,
+        fontsize: Optional[Any] = ...,
+        colormap: Optional[Any] = ...,
+        table: bool = ...,
+        yerr: Optional[Any] = ...,
+        xerr: Optional[Any] = ...,
+        label: Optional[Any] = ...,
+        secondary_y: bool = ...,
+        **kwds: Any
+    ): ...
     def line(self, **kwds: Any): ...
     def bar(self, **kwds: Any): ...
     def barh(self, **kwds: Any): ...
     def box(self, **kwds: Any): ...
     def hist(self, bins: int = ..., **kwds: Any): ...
-    def kde(self, bw_method: Optional[Any] = ..., ind: Optional[Any] = ..., **kwds: Any): ...
+    def kde(
+        self, bw_method: Optional[Any] = ..., ind: Optional[Any] = ..., **kwds: Any
+    ): ...
     density: Any = ...
     def area(self, **kwds: Any): ...
     def pie(self, **kwds: Any): ...
 
 class FramePlotMethods(BasePlotMethods):
-    def __call__(self, x: Optional[Any] = ..., y: Optional[Any] = ..., kind: str = ..., ax: Optional[Any] = ..., subplots: bool = ..., sharex: Optional[Any] = ..., sharey: bool = ..., layout: Optional[Any] = ..., figsize: Optional[Any] = ..., use_index: bool = ..., title: Optional[Any] = ..., grid: Optional[Any] = ..., legend: bool = ..., style: Optional[Any] = ..., logx: bool = ..., logy: bool = ..., loglog: bool = ..., xticks: Optional[Any] = ..., yticks: Optional[Any] = ..., xlim: Optional[Any] = ..., ylim: Optional[Any] = ..., rot: Optional[Any] = ..., fontsize: Optional[Any] = ..., colormap: Optional[Any] = ..., table: bool = ..., yerr: Optional[Any] = ..., xerr: Optional[Any] = ..., secondary_y: bool = ..., sort_columns: bool = ..., **kwds: Any): ...
+    def __call__(
+        self,
+        x: Optional[Any] = ...,
+        y: Optional[Any] = ...,
+        kind: str = ...,
+        ax: Optional[Any] = ...,
+        subplots: bool = ...,
+        sharex: Optional[Any] = ...,
+        sharey: bool = ...,
+        layout: Optional[Any] = ...,
+        figsize: Optional[Any] = ...,
+        use_index: bool = ...,
+        title: Optional[Any] = ...,
+        grid: Optional[Any] = ...,
+        legend: bool = ...,
+        style: Optional[Any] = ...,
+        logx: bool = ...,
+        logy: bool = ...,
+        loglog: bool = ...,
+        xticks: Optional[Any] = ...,
+        yticks: Optional[Any] = ...,
+        xlim: Optional[Any] = ...,
+        ylim: Optional[Any] = ...,
+        rot: Optional[Any] = ...,
+        fontsize: Optional[Any] = ...,
+        colormap: Optional[Any] = ...,
+        table: bool = ...,
+        yerr: Optional[Any] = ...,
+        xerr: Optional[Any] = ...,
+        secondary_y: bool = ...,
+        sort_columns: bool = ...,
+        **kwds: Any
+    ): ...
     def line(self, x: Optional[Any] = ..., y: Optional[Any] = ..., **kwds: Any): ...
     def bar(self, x: Optional[Any] = ..., y: Optional[Any] = ..., **kwds: Any): ...
     def barh(self, x: Optional[Any] = ..., y: Optional[Any] = ..., **kwds: Any): ...
     def box(self, by: Optional[Any] = ..., **kwds: Any): ...
     def hist(self, by: Optional[Any] = ..., bins: int = ..., **kwds: Any): ...
-    def kde(self, bw_method: Optional[Any] = ..., ind: Optional[Any] = ..., **kwds: Any): ...
+    def kde(
+        self, bw_method: Optional[Any] = ..., ind: Optional[Any] = ..., **kwds: Any
+    ): ...
     density: Any = ...
     def area(self, x: Optional[Any] = ..., y: Optional[Any] = ..., **kwds: Any): ...
     def pie(self, y: Optional[Any] = ..., **kwds: Any): ...
-    def scatter(self, x: Any, y: Any, s: Optional[Any] = ..., c: Optional[Any] = ..., **kwds: Any): ...
-    def hexbin(self, x: Any, y: Any, C: Optional[Any] = ..., reduce_C_function: Optional[Any] = ..., gridsize: Optional[Any] = ..., **kwds: Any): ...
+    def scatter(
+        self,
+        x: Any,
+        y: Any,
+        s: Optional[Any] = ...,
+        c: Optional[Any] = ...,
+        **kwds: Any
+    ): ...
+    def hexbin(
+        self,
+        x: Any,
+        y: Any,
+        C: Optional[Any] = ...,
+        reduce_C_function: Optional[Any] = ...,
+        gridsize: Optional[Any] = ...,
+        **kwds: Any
+    ): ...
